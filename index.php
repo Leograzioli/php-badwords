c03798b5241c3d08e7afc06e9ab4f11b6d4b02f9
Leograzioli <leograzioli@gmail.com>
2022-11-24 14:24:33 +0100

    add style css

--- index.php
+++ index.php
@@ -5,19 +5,28 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+
+    <link rel="stylesheet" href="css/style.css">
 </head>
 <body>
-    <form action="response.php" method="GET">
-
-        <label for="txt">input your text</label>
-        <textarea name="txt" id="txt"> </textarea>
+    <div class="container">
+        <form action="response.php" method="GET">
+            
+            <div class="text-input">
+                <label for="txt">input your text: </label>
+                <textarea name="txt" id="txt"> </textarea>
+            </div>
     
+            <div class="word-input">
+                <label for="censure">word to censure</label>
+                <input type="text" id="censure" name="censure" placeholder="exemple: fuck">
+            </div>    
     
-        <label for="censure">word to censure</label>
-        <input type="text" id="censure" name="censure">
-
-        <button type="submit">Send</button>
-    </form>
+            <div class="btn">
+                <button type="submit">Send</button>
+            </div>
+        </form>
+    </div>
     
 </body>
 </html>
